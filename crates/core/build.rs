abab94a95fce8cdece44b9eef4d504ebdbe3a268
Gaëtan André <rvlander@gaetanandre.eu>
2026-04-06 14:30:05 +0200

    build: link libcollatinus_wrapper in build.rs

--- crates/core/build.rs
+++ crates/core/build.rs
@@ -3,30 +3,31 @@ use std::env;
 fn main() {
     let target = env::var("TARGET").unwrap();
 
-    // Cross-compiling for Kobo.
     if target == "arm-unknown-linux-gnueabihf" {
         println!("cargo:rustc-env=PKG_CONFIG_ALLOW_CROSS=1");
         println!("cargo:rustc-link-search=target/mupdf_wrapper/Kobo");
+        println!("cargo:rustc-link-search=target/collatinus_wrapper/Kobo");
         println!("cargo:rustc-link-search=libs");
         println!("cargo:rustc-link-lib=dylib=stdc++");
-    // Handle the Linux and macOS platforms.
     } else {
         let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
         match target_os.as_ref() {
             "linux" => {
                 println!("cargo:rustc-link-search=target/mupdf_wrapper/Linux");
+                println!("cargo:rustc-link-search=target/collatinus_wrapper/Linux");
                 println!("cargo:rustc-link-lib=dylib=stdc++");
             },
             "macos" => {
                 println!("cargo:rustc-link-search=target/mupdf_wrapper/Darwin");
+                println!("cargo:rustc-link-search=target/collatinus_wrapper/Darwin");
                 println!("cargo:rustc-link-lib=dylib=c++");
             },
             _ => panic!("Unsupported platform: {}.", target_os),
         }
-
         println!("cargo:rustc-link-lib=mupdf-third");
     }
 
+    println!("cargo:rustc-link-lib=collatinus_wrapper");
     println!("cargo:rustc-link-lib=z");
     println!("cargo:rustc-link-lib=bz2");
     println!("cargo:rustc-link-lib=jpeg");
