bad665604c5f4442c2b1a7ee8142f2f162f1fb0e
Gaëtan André <rvlander@gaetanandre.eu>
2026-04-25 08:30:52 +0200

    feat(nix): add Rust stable toolchain with arm-unknown-linux-gnueabihf target

--- flake.nix
+++ flake.nix
@@ -1,18 +1,30 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url  = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url    = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       systems       = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems f;
     in {
       devShells = forAllSystems (system:
         let
-          pkgs   = import nixpkgs { inherit system; };
+          pkgs   = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
           linaro = import ./nix/linaro.nix { inherit pkgs system; };
+          rust   = pkgs.rust-bin.stable.latest.default.override {
+            targets = [ "arm-unknown-linux-gnueabihf" ];
+          };
         in {
           default = pkgs.mkShell {
-            packages = [ linaro ];
+            packages = [ linaro rust ];
           };
         }
       );
